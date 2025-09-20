a83705c2b5e8b6b8f4b8f0275f76e95352cbdbe7
Anson Sun <anson.sun@mail.utoronto.ca>
2025-09-19 21:46:57 -0400

    Activity 1.3 - Update Chapter 3 Example

--- hello.py
+++ hello.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from datetime import datetime, timezone
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+from flask_moment import Moment
+
 app = Flask(__name__)
+
+bootstrap = Bootstrap5(app)
+moment = Moment(app)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html',
+                           current_time=datetime.now(timezone.utc))
+
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
